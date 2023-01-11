4c111f934a8e5535ffbb40c58f5bd6130841d5a5
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-01-11 12:06:19 -0700

    Nice or Mean game and color
    added Colorama module

---
+++ nice_or_mean.py
@@ -0,0 +1,132 @@
+#
+# Python:   3.10.7
+#
+# Author:   Lucas K. English
+#
+# Purpose:  To make a text based game called nice or mean
+#
+#
+
+import colorama
+# Adds ability to change text color
+from colorama import init, Fore, Back
+colorama.init()
+
+
+
+def start(nice=0,mean=0,name=""):
+    # get user's name
+    name = describe_game(name)
+    nice, mean, name = nice_mean(nice,mean,name)
+
+
+def describe_game(name):
+    """
+        check if this is a new game or not.
+        if it is new, get the user's name.
+        if it is not a new game, thank the player for
+        playing again and continue with the game.
+    """
+    # meaning, if we do not already have this user's name,
+    # then they are a new player and we need to get their name
+    if name != "":
+        print(Fore.GREEN + "\nThank you for playing again, {}!".format(name))
+    else:
+        stop = True
+        while stop:
+            if name == "":
+                name = input(Fore.GREEN + "\nWhat is your name? \n>>> ").capitalize()
+                if name != "":
+                    print("\nWelcome, {}!".format(name))
+                    print("\nIn this game, you will be greeted \nby several different people. \nYou can choose to be nice or mean")
+                    print("but at the end of the game your fate \nWill be sealed by your actions.")
+                    stop = False
+    return name
+
+###
+
+def nice_mean(nice,mean,name):
+    stop = True
+    while stop:
+        show_score(nice,mean,name)
+        pick = input("\nA stranger approaches you for a \nconversation. Will you be nice \nor mean? (N/M) \n>>>: ".lower())
+        if pick == "n":
+            print("\nThe stranger walks away smiling...")
+            nice = (nice + 1)
+            stop = False
+        if pick == "m":
+            print("\nThe stranger glares at you \nmenacingly and storms off...")
+            mean = (mean + 1)
+            stop = False
+    score(nice,mean,name) # pass the 3 variables to the score()
+
+###
+
+def show_score(nice,mean,name): 
+    print("\n{}, your current total: \n(Nice = {})  (Mean = {})".format(name,nice,mean))
+
+
+def score(nice,mean,name):
+    # score function is being passed the values stored within the 3 variables
+    if nice > 2:    # if condition is valid, call win function passing in the variables so it can use them
+        win(nice,mean,name)
+    if mean > 2:    # if condition is valid, call lose function passing in the variables so it can use them
+        lose(nice,mean,name)
+    else:           # else, call nice_mean function passing in the variables so it can use them
+        nice_mean(nice,mean,name)
+
+
+
+def win(nice,mean,name):
+    # Subsitute the {} wildcards with our variable values
+    print("\nNice job {}, you win! \nEveryone loves you and you've \nmade lots of friends along the way!!".format(name))
+    # call again functions and pass in our variables
+    again(nice,mean,name)
+    
+
+def lose(nice,mean,name):
+    # Substitute the {} wildcards with our variable values
+    print("\nAhhh too bad, game over! \n{}, you live in a dirty beat-up \nvan by the river, wretched and alone:,(".format(name))
+    # call again functions and pass in our variables
+    again(nice,mean,name)
+    
+
+def again(nice,mean,name):
+    stop = True
+    while stop:
+        choice = input("\nDo you want to play again? (y/n):\n>>>:").lower()
+        if choice == "y":
+            stop = False
+            reset(nice,mean,name)
+        if choice == "n":
+            print("\nOh...okay, I see how it is...it's fine I guess......\nI didn't wanna play again anyway...")
+            stop = False
+            quit()
+        else:
+            print("\nEnter ( y ) for 'YES', ( n ) for 'NO':\n>>> ")
+
+
+def reset(nice,mean,name):
+    nice = 0
+    mean = 0
+    #Notice, I do not reset the name variable as that same user has elected to play again
+    start(nice,mean,name)
+            
+        
+
+        
+
+                    
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
+
