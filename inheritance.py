9180ab54be28425602ab16f5bbbb89d183c6609f
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-01-17 10:17:23 -0700

    Inheritance asng
    Created an animal class and two child classes

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+#
+# Python:   3.10.7
+#
+# Author:   Lucas K. English
+#
+# Purpose:  To demonstrate inheritance between parent and child classes
+#  
+#
+
+class Animal:
+    coat = 'Texture and color of animal'
+    life_span = 0
+    diet = 'What they eat'
+    activity_level = 'Energy level'
+
+class Cat(Animal):
+    wisker_count: 0
+    max_jump_height: 0
+
+class Dog(Animal):
+    breed: 'Type of dog'
+    job: 'What the dog does'
+    
+
+    
+    
+    
