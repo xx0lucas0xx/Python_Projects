b97e3ca2642a65ad34683c84e27a05964cd2467b
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-01-21 10:56:31 -0700

    fixed polymorphism syntax errors and tkinter

--- polymorphism.py
+++ polymorphism.py
@@ -8,11 +8,11 @@ class plant:
 
     # input plant to track
     def inputPlant(self):
-        entry_name = input("What is the name of the plant you'd like to track?\n>>>")
-        entry_climate = input("What's your plant's climate?\n>>>")
-        entry_sun_exposure = input("How much sun does your plant need?\n>>>")
+        entry_name = input("What is the name of the plant you'd like to track?\n>>> ")
+        entry_climate = input("What's your plant's climate?\n>>> ")
+        entry_sun_exposure = input("How much sun does your plant need?\n>>> ")
         if (entry_name != "" and entry_climate != "" and entry_sun_exposure != ""):
-            print(" {} needs {} enviornment and requires {} hours of sun per day".format(entry_name, entry_climate, entry_sun_exposure))
+            print(" {} needs a {} enviornment and requires {} hours of sun per day".format(entry_name, entry_climate, entry_sun_exposure))
 
 
 
@@ -24,10 +24,10 @@ class tree(plant):
     # same as plant entry but for trees
 
     def inputPlant(self):
-        entry_fruit = input("What's your tree's fruit?\n>>>")
-        entry_season = input("what season does your tree bloom in?\n>>>")
+        entry_fruit = input("What's your tree's fruit?\n>>> ")
+        entry_season = input("what season does your tree bloom in?\n>>> ")
         if (entry_fruit != "" and entry_season != ""):
-            print(" {} produces tasty {}'s and it blooms in {}".format(entry_fruit, entry_season))
+            print(" They grow tasty {} and they bloom in {}!".format(entry_fruit, entry_season))
 
 
 
@@ -40,10 +40,10 @@ class schrub(plant):
     # same as plant entry but for shrubs
 
     def inputPlant(self):
-        entry_flower = input("What's your schrub's flower?\n>>>")
-        entry_color = input("What color are your shrubs flowers?\n>>>")
+        entry_flower = input("What's your schrub's flower?\n>>> ")
+        entry_color = input("What color are your shrubs flowers?\n>>> ")
         if (entry_flower != "" and entry_color != ""):
-            print(" {} has {} that are {} and very beautiful".format(entry_flower, entry_color))
+            print(" They grow {} that are {} and very beautiful!".format(entry_flower, entry_color))
 
 
 if __name__ == "__main__":
