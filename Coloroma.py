70b96e3bfe88d34e31b81a4920f6d2a6ddf421a5
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-02-18 12:31:41 -0700

    commit

---
+++ Coloroma.py
@@ -0,0 +1,12 @@
+
+        
+import colorama
+
+from colorama import init, Fore, Back
+colorama.init()
+ 
+# Fore changes the text's foreground color
+print(Fore.BLUE + "Blue Letters")
+ 
+#Back changes the text's background color
+print(Back.WHITE + "White Background")
