8bbed28707a1650ae5b1d3e8ab2d1ffc92e44634
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-02-26 20:18:14 -0700

    Update Coloroma.py

--- Coloroma.py
+++ Coloroma.py
@@ -8,5 +8,5 @@ colorama.init()
 # Fore changes the text's foreground color
 print(Fore.BLUE + "Blue Letters")
  
-#Back changes the text's background color
+# Back changes the text's background color
 print(Back.WHITE + "White Background")
